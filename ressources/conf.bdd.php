4a7b12b6fa10c638b0b83610efcb759a5e1775e0
jYahyaDev <62561346+jYahyaDev@users.noreply.github.com>
2021-03-29 15:59:18 +0200

    Add files via upload

---
+++ ressources/conf.bdd.php
@@ -0,0 +1,8 @@
+<?php
+
+  define("DB_SERVER", "localhost");
+  define("DB_DATABASE", "country");
+  define("DB_USERNAME", "jyahya");
+  define("DB_PASSWORD", "Azerty30900!");
+
+?>
---
+++ ressources/site/page-pays-creer--traitement.php
@@ -0,0 +1,45 @@
+<?php
+  define("PAGE_TITLE", "Traitement");
+  require("inc/inc.kickstart.php");
+?>
+
+<main class="pays-creer">
+<?php
+  
+  $countryName = htmlspecialchars($_POST['country_name']);
+  $countryFlag = htmlspecialchars($_POST['country_flag']);
+  $countryCapital = htmlspecialchars($_POST['country_capital']);
+  $countryArea =htmlspecialchars($_POST["country_area"]);
+ 
+  var_dump($countryArea);
+
+  try{
+
+
+  $prepare = "INSERT INTO `country` (`country_name`, `country_flag`, `country_capital`, `country_area`) VALUES (:country_name,:country_flag,:country_capital,:country_area)";
+
+  $requete = $pdo->prepare($prepare);
+  $requete->execute([
+      ":country_name"=>$countryName,
+      ":country_flag"=>$countryFlag,
+      ":country_capital"=>$countryCapital,
+      ":country_area"=>number_format($countryArea)
+  ]);
+
+  echo "<h3>Merci !</h3>";
+  echo "<p>Voici un récapitulatif de votre contribution :</p>";
+  echo "<ul>"
+      ."<li>Nom du pays : " . $countryName . "</li>"
+      ."<li>Capitale du pays : " . $countryCapital . "</li>"
+      ."<li>Drapeau du pays : " . $countryFlag . "</li>"
+      ."<li>Superficie du pays (en km²) : " . $countryArea . "</li>"
+      ."<ul>";
+  echo "<a href='page-pays-liste-alpha.php'><button>Consulter la liste des pays</button></a>";
+  }catch (PDOException $e) {
+    echo "<pre>✖️ Erreur liée à la requête SQL :\n" . $e->getMessage() . "</pre>";
+  }
+ 
+?>
+</main>
+
+<?php require("inc/inc.footer.php"); ?>
